e7f4259f9c4fdbd6d90bc0376bfc1f46c178588a
LearningCitizen <johnson.hippolyte@yahoo.fr>
2018-11-21 11:56:08 +0100

    init spring initializr

---
+++ src/main/java/com/ja/OnlineShoppingStore/OnlineShoppingStoreApplication.java
@@ -0,0 +1,12 @@
+package com.ja.OnlineShoppingStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingStoreApplication.class, args);
+	}
+}
