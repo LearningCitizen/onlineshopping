a701ef597348eef8d24716f4bb6505e5bb04afe3
aelazhari <elazhari.abdelaziz@gmail.com>
2018-11-21 16:05:05 +0100

    configuring databases relationship jpa hibernate spring

---
+++ ossbe/src/main/java/com/ja/ossbe/app/OssbeApplication.java
@@ -0,0 +1,13 @@
+package com.ja.ossbe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssbeApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(OssbeApplication.class, args);
+        }
+
+}
