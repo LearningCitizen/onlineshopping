985a847653de412a9cf86ddcb75a2dda9516889a
aelazhari <elazhari.abdelaziz@gmail.com>
2018-11-22 11:41:01 +0100

    Config 1 Success

--- ossbe/src/main/java/com/ja/ossbe/app/OssbeApplication.java
+++ ossbe/src/main/java/com/ja/ossbe/app/OssbeApplication.java
@@ -1,9 +1,11 @@
 package com.ja.ossbe.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class OssbeApplication {
 
     public static void main(String[] args) {
