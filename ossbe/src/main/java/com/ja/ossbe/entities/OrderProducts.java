9b05faa23ce1001e90041299919f00ec4a0d10ab
LearningCitizen <johnson.hippolyte@yahoo.fr>
2018-11-26 14:40:53 +0100

    Backend - add foreign keys in OrderProducts

--- ossbe/src/main/java/com/ja/ossbe/entities/OrderProducts.java
+++ ossbe/src/main/java/com/ja/ossbe/entities/OrderProducts.java
@@ -3,12 +3,18 @@ package com.ja.ossbe.entities;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "order_products")
 public class OrderProducts {
-    private Integer orderId;
-    private Integer productId;
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
 }
