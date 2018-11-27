11a3c044c2179c025de7b36ba9e150843f908ac8
LearningCitizen <johnson.hippolyte@yahoo.fr>
2018-11-27 11:25:34 +0100

    Front End - creation of the route in app.module

--- ossfe/src/app/app.module.ts
+++ ossfe/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
